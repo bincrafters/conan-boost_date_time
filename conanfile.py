ddec01943c35276da70b1d9b5972d18b46232385
Jerry W <jerrywiltse@gmail.com>
2017-08-09 00:36:52 -0400

    successfully built and published to conan

--- conanfile.py
+++ conanfile.py
@@ -3,14 +3,11 @@ from conans import ConanFile, tools, os
 class BoostDate_TimeConan(ConanFile):
     name = "Boost.Date_Time"
     version = "1.64.0"
-    generators = "txt"
+    generators = "boost"
     url = "https://github.com/boostorg/date_time"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     requires =  "Boost.Level11Group/1.64.0@bincrafters/testing"
-   
-    def package_id(self):
-        self.info.header_only()
         
     #This library is part of one or more cyclic dependency groups within Boost.
     
