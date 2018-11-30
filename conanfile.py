da7fe49899aa436f55ab31a704964ecc16dcee6b
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:11 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,11 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostDate_TimeConan(base.BoostBaseConan):
     name = "boost_date_time"
     url = "https://github.com/bincrafters/conan-boost_date_time"
     lib_short_names = ["date_time"]
-    cycle_group = "boost_level11group"
-    b2_requires = [
-        "boost_level11group",
-    ]
+    cycle_group = "boost_cycle_group_c"
+    b2_requires = ["boost_cycle_group_c"]
